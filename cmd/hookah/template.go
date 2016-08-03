9edb399dc79743653fb883dd671ac3bd66aeb677
Hiram J. Pérez <worg@linuxmail.org>
2016-08-03 16:51:54 -0500

    format commit messages, prevents escaping issues

--- cmd/hookah/template.go
+++ cmd/hookah/template.go
@@ -4,7 +4,7 @@ const (
 	msgTmpl = `
 {{.hook.Author.Name}} pushed {{if eq (len .hook.Commits) 0 }}a new branch: {{.branch}}{{else}}{{.hook.Commits | len}} commit[s] to [{{.hook.Repo.Name}}:{{.branch}}]({{.hook.Repo.URL}}/tree/{{.branch}}){{end}}
 {{range .hook.Commits}}{{$id := (.ID  | printf "%.7s")}}
-    [{{$id}}]({{.URL}}): {{ trimSpace .Message | printf "%.80s"  }}{{if gt (len .Message) 79 }}…{{end}} — {{if .Author.Name}}{{.Author.Name}}{{else}}{{.Author.Username}}{{end}}{{/* 
+    [{{$id}}]({{.URL}}): {{ trimSpace .Message | printf "%.80s" | fmtCommit  }}{{if gt (len .Message) 79 }}…{{end}} — {{if .Author.Name}}{{.Author.Name}}{{else}}{{.Author.Username}}{{end}}{{/* 
     no newline between commits
 */}}{{end}}
 `
